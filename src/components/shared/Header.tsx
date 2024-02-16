89c85ea85c4abbb41580737b508a51e2951017ba
Dinaerte Neto <dinaerteneto@gmail.com>
2024-02-16 16:21:05 -0300

    feat: add popovers for header icons

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,4 +1,7 @@
-import { HiOutlineSearch } from 'react-icons/hi'
+import { Popover, Transition } from '@headlessui/react'
+import classNames from 'classnames'
+import { Fragment } from 'react'
+import { HiOutlineBell, HiOutlineChatAlt, HiOutlineSearch } from 'react-icons/hi'
 
 function Header() {
     return (
@@ -11,7 +14,73 @@ function Header() {
                     className="text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-300 rounded-sm px-4 pl-11"
                 />
             </div>
-            <div></div>
+            <div className="flex items-center gap-2 mr-2">
+                <Popover>
+                    {({ open }) => (
+                        <>
+                            <Popover.Button
+                                className={classNames(
+                                    open && 'bg-gray-100',
+                                    'p-1.5 rounded-sm inline-flex item-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100'
+                                )}
+                            >
+                                <HiOutlineChatAlt fontSize={24} />
+                            </Popover.Button>
+
+                            <Transition
+                                as={Fragment}
+                                enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0"
+                            >
+                                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                                    <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                                        <strong className="text-gray-700 font-medium">Messages</strong>
+                                        <div className="mt-2 py-1 text-sm">this is message panel</div>
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </>
+                    )}
+                </Popover>
+
+                <Popover>
+                    {({ open }) => (
+                        <>
+                            <Popover.Button
+                                className={classNames(
+                                    open && 'bg-gray-100',
+                                    'p-1.5 rounded-sm inline-flex item-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100'
+                                )}
+                            >
+                                <HiOutlineBell fontSize={24} />
+                            </Popover.Button>
+
+                            <Transition
+                                as={Fragment}
+                                enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0"
+                            >
+                                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                                    <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                                        <strong className="text-gray-700 font-medium">Notifications</strong>
+                                        <div className="mt-2 py-1 text-sm">this is notification panel</div>
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </>
+                    )}
+                </Popover>                
+
+                
+            </div>
         </div>
     )
 }
